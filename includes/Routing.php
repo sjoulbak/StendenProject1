1ba03c74055aa869098246d1943770c8c8e0aa9c
alwintje <Alwin12345>
2015-10-09 09:21:38 +0200

    php info

--- includes/Routing.php
+++ includes/Routing.php
@@ -5,3 +5,12 @@
  * Date: 9-10-2015
  * Time: 09:10
  */
+require_once("AltoRouter.php");
+$router = new AltoRouter();
+
+// map homepage
+$router->map( 'GET', '/nl/', function() {
+    require __DIR__ . '../nl.txt';
+
+});
+var_dump($router->getRoutes());
