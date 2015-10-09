f8e78d3f7f6efa5981c2dffb7988c394f8bfcac5
alwintje <Alwin12345>
2015-10-09 13:01:21 +0200

    languages

--- languages/en_EN.php
+++ languages/en_EN.php
@@ -13,11 +13,41 @@ function homePage(){
                 </article>
             ";
 }
+
 function educationsPage(){
     echo "<h1>Educations</h1>";
     echo "
                 <article>
-                    Educations
+                    <h3>Sports Psychology</h3>
+                    <p>What you do in the sport, the Sport Psychology definitely opens your eyes to the interesting psychological aspects of sport behavior. And that can be very useful. If you coach athletes and get them to guide even better. Or for yourself, if you for instance do fitness or running, and want to give that little bit extra.
+In any sport form man body, mind and spirit a whole. To book sporting success, all three elements are in balance. Besides the necessary physical fitness is also the mental state of an athlete determines his health and his performance. Precisely thereby sports psychology is concerned. Upon completion of this course you know how sport performance is linked to the human psyche.
+Contents of the training course Sports Psychology
+Training Sports psychology is a home study and consists of ten modules. The course material consists of many practical exercises, research and information from case studies. For the Sport Psychology is no special training. You get a complete overview of the current state of affairs in the psychology of sport behavior and performance. Thanks exercises under the banner How do you stand it? At the end of each chapter, you will direct more self-awareness. You are constantly encouraged to apply the suggestions and opinions from every class as much as possible. Thus, following the training Sports psychology really a living experience for you. During the training the following topics:
+</p>
+                    <p>
+                        De opleiding Sportpsychologie is een thuisstudie en bestaat uit tien modules. Het lesmateriaal bestaat uit veel praktische oefeningen, onderzoek en informatie uit casestudies. Voor de opleiding Sportpsychologie is geen speciale vooropleiding nodig. Je krijgt een volledig overzicht van de huidige stand van zaken in de psychologie van sportgedrag en prestatie. Dankzij oefeningen onder het motto Hoe sta je ervoor? aan het slot van ieder hoofdstuk, krijg je direct meer zelfbewustzijn. Je wordt voortdurend aangespoord om de suggesties en adviezen uit iedere les zo veel mogelijk toe te passen. Zo wordt het volgen van de opleiding Sportpsychologie ook echt een levende ervaring voor je. Tijdens de opleiding komen de volgende onderwerpen aan bod:
+                        <ul>
+                        <li>The discipline vetted</li>
+                        <li>A healthy mind in a healthy body</li>
+                        <li>Psychology and sports performance</li>
+                        <li>Youth (top) sport</li>
+                        <li>Skill and top performance</li>
+                        <li>The profile of the champion</li>
+                        <li>The art of coaching</li>
+                        <li>Psychological Skills Training (PST)</li>
+                        <li>The (top) sports</li>
+                        <li>The mental training program</li>
+                        </ul>
+                    </p>
+                    <strong>By studying?</strong>
+                    <p>
+                       Have you completed the Sport Psychology / Mental Coach and would you like to develop further? The programs at divisional Coaching & Management are the perfect follow-up courses. It is also possible to develop your in width by following additional training in the Fitness Academy.
+                    </p>
+                    <strong>Examination</strong><br />
+                    <p>
+                    You pass the Sport Psychology if you have earned a passing grade for all assignments.
+
+                   </p>
                 </article>
             ";
 }
--- languages/nl_NL.php
+++ languages/nl_NL.php
@@ -15,10 +15,32 @@ function homePage(){
             ";
 }
 function educationsPage(){
-        echo "<h1>Educations</h1>";
+        echo "<h1>Opleidingen</h1>";
         echo "
                 <article>
-                    Educations
+                    <h3>Sportpsychologie</h3>
+                    <p>Wat je ook doet in de sport, de opleiding Sportpsychologie opent beslist je ogen voor de boeiende psychologische aspecten van sportgedrag. En dat kan erg nuttig zijn. Als je sporters coacht en je ze nog beter wilt begeleiden. Of voor jezelf, als je bijvoorbeeld aan fitness doet of hardloopt en net dat beetje extra wilt geven.
+In ieder sportmens vormen lichaam, geest en ziel één geheel. Om sportief succes te boeken, moeten alle drie de elementen in balans zijn. Naast de noodzakelijke lichamelijke conditie is ook de mentale gesteldheid van een sporter bepalend voor zijn welzijn en zijn prestatie. Juist daarmee houdt de sportpsychologie zich bezig. Na voltooiing van deze opleiding weet je hoe sportprestaties zijn verbonden met de menselijke psyche.
+</p>
+                    <p>
+                        De opleiding Sportpsychologie is een thuisstudie en bestaat uit tien modules. Het lesmateriaal bestaat uit veel praktische oefeningen, onderzoek en informatie uit casestudies. Voor de opleiding Sportpsychologie is geen speciale vooropleiding nodig. Je krijgt een volledig overzicht van de huidige stand van zaken in de psychologie van sportgedrag en prestatie. Dankzij oefeningen onder het motto Hoe sta je ervoor? aan het slot van ieder hoofdstuk, krijg je direct meer zelfbewustzijn. Je wordt voortdurend aangespoord om de suggesties en adviezen uit iedere les zo veel mogelijk toe te passen. Zo wordt het volgen van de opleiding Sportpsychologie ook echt een levende ervaring voor je. Tijdens de opleiding komen de volgende onderwerpen aan bod:
+                        <ul>
+                        <li>De discipline doorgelicht</li>
+                        <li>Een gezonde geest in een gezond lichaam</li>
+                        <li>Psychologie en sportprestatie</li>
+                        <li>Jeugd(top)sport</li>
+                        <li>Vaardigheid en topprestatie</li>
+                        <li>Het profiel van de kampioen</li>
+                        <li>De kunst van het coachen</li>
+                        <li>Psychological Skills Training (PST)</li>
+                        <li>De (top)sportwereld</li>
+                        <li>Het mental trainingprogramma</li>
+                        </ul>
+                    </p>
+                    <strong>Doorstuderen?</strong>
+                    <p>
+                        Heb je de opleiding Sportpsychologie / Mental Coach afgerond en wil je je verder ontwikkelen? De opleidingen van de divisie Coaching & Management zijn de perfecte vervolgopleidingen. Tevens is het mogelijk om je in de breedte te ontwikkelen door aanvullende opleidingen te volgen binnen de Fitness Academy.
+                    </p>
                 </article>
             ";
 }
