c7f28b97854bb0f8981259eb84fa373424a7b204
alwintje <Alwin12345>
2015-10-09 11:39:19 +0200

    languages

--- includes/Language.php
+++ includes/Language.php
@@ -5,14 +5,18 @@
  * Date: 9-10-2015
  * Time: 09:10
  */
-if(isset($_GET['lang'])){
+
+
+if(isset($_GET['lang'])){ // Wanneer er een taal gekozen is, wordt hij hier veranderd
     $lang = $_GET['lang'];
     $_SESSION['lang'] = $_GET['lang'];
-}elseif(isset($_SESSION['lang'])){
+}elseif(isset($_SESSION['lang'])){ // Kijkt of er een sessie bestaat met een gekozen taal en gebruikt dan die taal.
     $lang = $_SESSION['lang'];
-}else{
+}else{ // Als er geen taal is gekozen wordt er gekeken naar de taal van de browser.
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 }
+
+// Verwerkt de talen.
 switch ($lang){
     case "nl":
         require_once("languages/nl_NL.php");
@@ -27,4 +31,3 @@ switch ($lang){
         $feed = "en_en";
         break;
 }
-//var_dump($router->getRoutes());
--- includes/Weather.php
+++ includes/Weather.php
@@ -10,11 +10,12 @@ class Weather{
 
     private $url = "http://api.openweathermap.org/data/2.5/weather?q=";
     private $location = "";
+    // API Key voor het weer
     private $apiKey = "714e4e703a5ce146a1fb3e8b0705743a";
     // units=metric zet temperatuur in graden celcius.
     private $urlParams = [
         "units" => "metric"
-    ];//"&units=metric";
+    ];
 
     public function setLocation($location){
         $this->location = $location;
@@ -22,13 +23,16 @@ class Weather{
     public function getLocation(){
         return $this->location;
     }
+
+    // Krijgt het weer terug
     public function getWeather(){
 
-        //
+
         $urlParams = "";
         foreach($this->urlParams as $key => $val){
             $urlParams .= "&$key=$val";
         }
+        // Maakt de URL goed
         $url = $this->url."{".urlencode($this->location)."}".$urlParams."&APPID=".$this->apiKey;
 
         $data = file_get_contents($url);
