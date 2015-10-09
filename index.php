2250052c27775b1ff9d7e2fad7e111d510098594
sjoulbak <remco_meinen@hotmail.com>
2015-10-09 10:44:18 +0200

    Modified Index body

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
   <title><?php echo $businessName; ?></title>
   <!-- Bootstrap -->
    <link href="css/bootstrap.min.css" rel="stylesheet">
+   <link href="css/index_body.css" rel="stylesheet">
 
    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -39,42 +40,48 @@
     </div>
   </header>
   <div class="container">
-    <h1><?php echo $businessName; ?></h1>
-    <a href="?lang=en"><img src="images/en.png" /></a>
-    <a href="?lang=nl"><img src="images/nl.png" /></a>
-    <?php
-    // Feed channel laten zien
-    $channel = $feed->getChannel();
-    echo "<h2><a href='".$channel->link."' target='_blank'>".$channel->title."</a></h2>";
-    echo "<br />";
+    <section class="top">
 
-    // Feed items laten zien
-    foreach($feed->getItems() as $val){
-        echo "<p>";
-        echo $val->title;
-        echo "<br />";
-        echo $val->description;
-        echo "</p>";
-    }
+    </section>
+    <section class="mid">
 
-    // Temperatuur en zonsopkomstt in Meppel
-    $weather->setLocation("Meppel");
-    $w = $weather->getWeather();
-    $sunrise = new DateTime();
-    $sunrise->setTimezone(new DateTimeZone('europe/amsterdam'));
-    $sunrise->setTimestamp($w->sys->sunrise);
+    </section>
+    <section class="bottom">
+      <!-- <h1><?php echo $businessName; ?></h1>
+      <a href="?lang=en"><img src="images/en.png" /></a>
+      <a href="?lang=nl"><img src="images/nl.png" /></a>
+      <?php
+      // Feed channel laten zien
+      $channel = $feed->getChannel();
+      echo "<h2><a href='".$channel->link."' target='_blank'>".$channel->title."</a></h2>";
+      echo "<br />";
 
-    echo "<p>";
-    echo "Graden: ".$w->main->temp." &deg; <br />";
-    echo "Zonsopkomst: ".$sunrise->format('H:i:s');
-    echo "</p>";
+      // Feed items laten zien
+      foreach($feed->getItems() as $val){
+          echo "<p>";
+          echo $val->title;
+          echo "<br />";
+          echo $val->description;
+          echo "</p>";
+      }
 
-    ?>
+      // Temperatuur en zonsopkomstt in Meppel
+      $weather->setLocation("Meppel");
+      $w = $weather->getWeather();
+      $sunrise = new DateTime();
+      $sunrise->setTimezone(new DateTimeZone('europe/amsterdam'));
+      $sunrise->setTimestamp($w->sys->sunrise);
+
+      echo "<p>";
+      echo "Graden: ".$w->main->temp." &deg; <br />";
+      echo "Zonsopkomst: ".$sunrise->format('H:i:s');
+      echo "</p>";
+
+      ?> -->
+    </section>
   </div>
   <footer>
-    <div class="container">
 
-    </div>
   </footer>
 </body>
 </html>
