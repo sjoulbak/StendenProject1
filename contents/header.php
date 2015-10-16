9b713e3a0d5d72ed9b3fd1fa33287b5c4f08c417
alwintje <Alwin12345>
2015-10-16 11:22:21 +0200

    languages

--- contents/header.php
+++ contents/header.php
@@ -1,8 +1,16 @@
 <header>
   <div class="lang">
     <ul>
-      <li><a href="<?php echo $langUrl; ?>lang=en"><img src="images/en.png" /></a></li>
-      <li><a href="<?php echo $langUrl; ?>lang=nl"><img src="images/nl.png" /></a></li>
+      <li>
+        <a href="<?php echo $langUrl; ?>lang=en">
+          <img src="images/en.png" />
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo $langUrl; ?>lang=nl">
+          <img src="images/nl.png" />
+        </a>
+      </li>
       <?php
         echo "<li>Graden: ".$w->main->temp." &deg;  </li>";
         echo "<li>Zonsopkomst: ".$sunrise->format('H:i:s')."</li>";
